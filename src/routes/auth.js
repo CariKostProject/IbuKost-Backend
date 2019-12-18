a2b5ffd71621885b9c274b31e7c7a8ef6074b772
metagenes <48299631+metagenes@users.noreply.github.com>
2019-12-18 17:10:06 +0700

    deploy

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,9 +4,9 @@ const AuthController = require('../controllers/auth');
 
 Route.get('/', (req, res) => {
 	res.json({
-		message: 'Welcome to PapiKost API',
+		message: 'Welcome to IbuKost API',
 		login: 'If you already have an account, please login',
-		register: 'Register your account today to use Papikost API'
+		register: 'Register your account today to use IbuKost API'
 	});
 })
 	.post('/register/user', AuthController.registerUser)
